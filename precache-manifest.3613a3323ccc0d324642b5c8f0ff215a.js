adcef62d54c4351843a3c11fc3acb69de0cd8c2f
Utkarsh Saxena <little@Utkarsh-Saxena.local>
2022-10-12 09:05:59 +0530

    Updates

---
+++ precache-manifest.3613a3323ccc0d324642b5c8f0ff215a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f235a5d8065c47f80bac5c1918210f3",
+    "url": "/ipod/index.html"
+  },
+  {
+    "revision": "c353e7d35b87f7366c6c",
+    "url": "/ipod/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b35e464241c540dd489e",
+    "url": "/ipod/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c353e7d35b87f7366c6c",
+    "url": "/ipod/static/js/2.5099411e.chunk.js"
+  },
+  {
+    "revision": "3afc3f7b055b07aac8edf61359703488",
+    "url": "/ipod/static/js/2.5099411e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b35e464241c540dd489e",
+    "url": "/ipod/static/js/main.c2ecff5e.chunk.js"
+  },
+  {
+    "revision": "e6acf8b982710a9850e8",
+    "url": "/ipod/static/js/runtime-main.59161aa3.js"
+  },
+  {
+    "revision": "45187f6493866d0a42b6a08e6ed9270f",
+    "url": "/ipod/static/media/Despacito.45187f64.jpg"
+  },
+  {
+    "revision": "a8772a8bc44986c7589dde8c70206a80",
+    "url": "/ipod/static/media/Despacito.a8772a8b.mp3"
+  },
+  {
+    "revision": "1e5d4d9fc4cb7db55133d115663a75aa",
+    "url": "/ipod/static/media/OnMyWay.1e5d4d9f.mp3"
+  },
+  {
+    "revision": "3d508d6a79eae336df8629c89eafaaf2",
+    "url": "/ipod/static/media/OnMyWay.3d508d6a.jpg"
+  },
+  {
+    "revision": "9ba16188fa31aa053935b27151e30f78",
+    "url": "/ipod/static/media/Senorita.9ba16188.mp3"
+  },
+  {
+    "revision": "f880c9df6471d3e99605c5ecdceddcf2",
+    "url": "/ipod/static/media/Senorita.f880c9df.webp"
+  },
+  {
+    "revision": "3e2eaebd3de6d930376a287d600450e7",
+    "url": "/ipod/static/media/author.3e2eaebd.jpg"
+  }
+]);
